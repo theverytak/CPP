58f203205cf9e3ddfb2152e4a424a64fd9aa64dd
J.Victor <thetak1123@gmail.com>
2016-07-14 01:39:11 +0900

    removing needs to be fixed

--- hw02/reply_admin.cc
+++ hw02/reply_admin.cc
@@ -86,19 +86,38 @@ int main(void) {
           // if there is a ',' in numbers;
           // 1. find the num; 2. erase it; 3. remake the string numbers;
           if(numbers.find(',') != string::npos) {
-            while(numbers.find(',') != string::npos) {
-              string str_num = numbers.substr(numbers.begin(), numbers.find(',') - numbers.begin());
+            int comma_index_start = 0, comma_index_end = 0;
+            while(comma_index_end != numbers.length()) {
+              // find the ','. if it's found, save the index, else make it the length of numbers so that we can exit the while loop;
+              for(int i = comma_index_start; i < numbers.length(); i++) {
+                if(numbers[i] == ',') {
+                  comma_index_end = i;
+                  break;
+                }
+                if(i == numbers.length() - 1) {
+                  comma_index_end = numbers.length();
+                }
+              }
+              string str_num = numbers.substr(comma_index_start, comma_index_end - comma_index_start);
               int num = stoi(str_num);
               is_removed = RemoveChat(chats, num);
-              numbers.erase(numbers.begin(), num.length() + 1);
+              comma_index_start = comma_index_end + 1;
             }
           }
           else if(numbers.find('-') != string::npos) {
             // find num1 and num2 from string numbers;
-            string str_num1 = numbers.substr(numbers.begin(), numbers.find('-'));
+            int dash_index;
+            for(int i = 0; i < numbers.length(); i++) {
+              if(numbers[i] == '-') {
+                dash_index = i;
+                break;
+              }
+            }
+            string str_num1 = numbers.substr(0, dash_index);
             string str_num2 = numbers.substr(numbers.find('-') + 1);
             int num1 = stoi(str_num1);
             int num2 = stoi(str_num2);
+            cout << "num : " << num1 << " " << num2 << endl;
             for(int i = num1; i <= num2; i++) {
               is_removed = RemoveChat(chats, i);
             }
