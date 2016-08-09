8d304647c45bddc05d8ed7e8524b8c8266d89400
theverytak <thetak1123@gmail.com>
2016-08-09 14:12:49 +0900

    Update bank_account.cc

--- hw10/bank_account.cc
+++ hw10/bank_account.cc
@@ -4,63 +4,64 @@
 #include "bank_account.h"
 
 unsigned int Account::ComputeExpectedBalance(unsigned int n_years_later) const {
-  return balance() + balance() * interest_rate() * n_years_later;
+	return balance() + balance() * interest_rate() * n_years_later;
 }
 
 unsigned int SavingAccount::ComputeExpectedBalance(unsigned int n_years_later) const {
-  int result = balance();
+	double result = balance();
 
-  for(int i = 0; i < n_years_later; i++)
-    result += balance() * interest_rate();
+	for (int i = 0; i < n_years_later; i++)
+		result += result * interest_rate();
 
-  return result;
+	return result;
 }
 
 bool SaveAccounts(const vector<Account*>& accounts, const string& filename) {
-  ofstream outfile;
-  outfile.open(filename);
+	ofstream outfile;
+	outfile.open(filename.c_str());
 
-  if(!outfile.good()) {
-    outfile.close();
+	if (!outfile.good()) {
+		outfile.close();
 
-    return false;
-  }
+		return false;
+	}
 
-  for(auto it = accounts.begin(); it != accounts.end(); it++)
-    outfile << it->name() << "\t" << it->type() << "\t"
-        << it->balance() << "\t" << it->interest_rate() << endl;
+	for (int i = 0; i < accounts.size(); i++)
+		outfile << accounts[i]->name() << "\t" << accounts[i]->type() << "\t"
+		<< accounts[i]->balance() << "\t" << accounts[i]->interest_rate() << endl;
 
-  outfile.close();
-
-  return true;
+	outfile.close();
+	
+	return true;
 }
 
 bool LoadAccounts(const string& filename, vector<Account*>* accounts) {
-  ifstream infile;
-  infile.open(filename);
+	ifstream infile;
+	infile.open(filename.c_str());
 
-  if(!infile.good()) {
-    infile.close();
+	if (!infile.good()) {
+		cout << "file open failure" << endl;
+		infile.close();
 
-    return false;
-  }
+		return false;
+	}
 
-  string name, type;
-  int balance;
-  double rate;
-  Account* ac;
+	string name, type;
+	int balance;
+	double rate;
+	Account* ac;
 
-  ifstream >> name >> type >> balance >> rate;
+	infile >> name >> type >> balance >> rate;
 
-  if(type == "checking")
-    ac = new Account(name, balance, rate);
+	if (type == "checking")
+		ac = new Account(name, balance, rate);
 
-  else if(type == "saving")
-    ac = new Account(name, balance, rate);
+	else if (type == "saving")
+		ac = new Account(name, balance, rate);
 
-  accounts->push_back(ac);
+	accounts->push_back(ac);
 
-  infile.close();
+	infile.close();
 
-  return true;
+	return true;
 }
